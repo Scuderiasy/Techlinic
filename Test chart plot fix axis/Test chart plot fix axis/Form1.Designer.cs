dae33c886332ca7e7616e7fe7f508680cf56babc
Scuderiasy <40252740+Scuderiasy@users.noreply.github.com>
2019-09-24 21:23:29 +0700

    Add files via upload

---
+++ Test chart plot fix axis/Test chart plot fix axis/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Test_chart_plot_fix_axis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.incoming_data_timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // incoming_data_timer
+            // 
+            this.incoming_data_timer.Interval = 250;
+            this.incoming_data_timer.Tick += new System.EventHandler(this.incoming_data_timer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Demonstrate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Enabled = false;
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(438, 339);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 429);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer incoming_data_timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Test chart plot fix axis/Test chart plot fix axis/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_chart_plot_fix_axis
+{
+    public partial class Form1 : Form
+    {
+        double a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+        double[] aa = new double[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            incoming_data_timer.Enabled = true;
+            incoming_data_timer.Start();
+        }
+
+        private void incoming_data_timer_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int incoming_data = random.Next(1, 20);
+            /*
+            a1 = a2;
+            a2 = a3;
+            a3 = a4;
+            a4 = a5;
+            a5 = a6;
+            a6 = a7;
+            a7 = a8;
+            a8 = a9;
+            a9 = a10;
+            a10 = incoming_data;
+
+            this.chart1.Series["Series1"].Points.Clear();
+
+            this.chart1.Series["Series1"].Points.AddXY(1, a1);
+            this.chart1.Series["Series1"].Points.AddXY(2, a2);
+            this.chart1.Series["Series1"].Points.AddXY(3, a3);
+            this.chart1.Series["Series1"].Points.AddXY(4, a4);
+            this.chart1.Series["Series1"].Points.AddXY(5, a5);
+            this.chart1.Series["Series1"].Points.AddXY(6, a6);
+            this.chart1.Series["Series1"].Points.AddXY(7, a7);
+            this.chart1.Series["Series1"].Points.AddXY(8, a8);
+            this.chart1.Series["Series1"].Points.AddXY(9, a9);
+            this.chart1.Series["Series1"].Points.AddXY(10, a10);
+            */
+            
+            /*
+            aa[0] = aa[1];
+            aa[1] = aa[2];
+            aa[2] = aa[3];
+            aa[3] = aa[4];
+            aa[4] = aa[5];
+            aa[5] = aa[6];
+            aa[6] = aa[7];
+            aa[7] = aa[8];
+            aa[8] = aa[9];
+            aa[9] = incoming_data;
+            */
+
+            
+            for (int bb = 0; bb < 9; bb++)
+            {
+                aa[bb] = aa[bb + 1];
+            }
+
+            aa[9] = incoming_data;
+
+            this.chart1.Series["Series1"].Points.Clear();
+
+            for(int cc = 0; cc<=9; cc++)
+            {
+                this.chart1.Series["Series1"].Points.AddXY(cc + 1, aa[cc]);
+            }
+        }
+    }
+}
